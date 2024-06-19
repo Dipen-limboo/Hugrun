8d0687cfb51e62f15df44fceeac12ef2aa7c9575
Dipen Limbu <dipenlimboo564@gmail.com>
2024-06-20 00:17:39 +0545

    mobile responsiveness

--- js/hugrun.js
+++ js/hugrun.js
@@ -1,3 +1,17 @@
+
+function changeLanguage() {
+    const select = document.getElementById('language-select');
+    const selectedOption = select.options[select.selectedIndex];
+    const newFlagSrc = selectedOption.getAttribute('data-img');
+    const newLangText = selectedOption.value.toUpperCase();
+
+    const currentFlag = document.getElementById('current-flag');
+    const currentLangText = document.getElementById('current-lang-text');
+
+    currentFlag.src = newFlagSrc;
+    currentFlag.alt = selectedOption.text;
+    currentLangText.textContent = newLangText;
+}
 document.addEventListener("DOMContentLoaded", function () {
     const mobileMenuIcon = document.querySelector(".mobile-menu-icon");
     const mobileCloseIcon = document.querySelector(".mobile-close-icon");
