ccb37e098b243293961f4fcd49ef8c728e54ab9a
Dipen Limbu <dipenlimboo564@gmail.com>
2024-06-20 00:15:58 +0545

    mobile responsive

---
+++ js/hugrun.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const mobileMenuIcon = document.querySelector(".mobile-menu-icon");
+    const mobileCloseIcon = document.querySelector(".mobile-close-icon");
+    const mobileNavContent = document.querySelector(".mobile-nav-content");
+    function closeMobileNav() {
+        mobileNavContent.style.display = "none";
+    }
+    mobileMenuIcon.addEventListener("click", function () {
+        mobileNavContent.style.display = "flex";
+    });
+    mobileCloseIcon.addEventListener("click", function () {
+        closeMobileNav();
+    });
+    const mobileNavLinks = document.querySelectorAll(".mobile-nav-content a");
+    mobileNavLinks.forEach(function (link) {
+        link.addEventListener("click", function () {
+            closeMobileNav();
+        });
+    });
+});
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+        }
+    }
+    }
+}
